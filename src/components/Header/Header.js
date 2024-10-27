a2a839e56514d4b8276d55909b53adbe04188cc8
arindom10 <karindom7@gmail.com>
2024-10-27 15:54:11 +0600

    header link setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,19 @@ import logo from "../../images/Logo.svg";
 import "./Header.css";
 
 const Header = () => {
-    return (
-        <nav className="header">
-            <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/about">About</Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
+      <div>
+        <Link to="/">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
